75586f75b39f4e6c8a213dc7f18d9e7650674d32
M08104 <guillaume.alfredo@ext.bpifrance.fr>
2022-11-01 08:11:09 +0100

    add docstrings + sql requests

--- src/test_graph/graph/compute.py
+++ src/test_graph/graph/compute.py
@@ -6,7 +6,13 @@ from test_graph.graph.utils import find_mention, get_journal_infos, get_article_
 
 
 def create_dict_infos(pubmed: pd.DataFrame, trials: pd.DataFrame, drug_str: str) -> Dict[str, Any]:
-    """performs all steps required to compute one drug relationships within the graph
+    """performs all steps required to compute one drug relationships within the graph :
+    1. Find pubmed articles that mention the drug
+    2. Find clinical trials aticles that mention the drug
+    3. Get all infos as dict from journals mentioning the drug, whether from pubmed or trials data
+    4. Get all infos as dict from articles mentioning the drug
+    5. Store info in a dict with keys pubmed_articles, journal, trials
+
 
     :param pubmed: pubmed dataframe
     :type pubmed: pd.DataFrame
@@ -19,9 +25,9 @@ def create_dict_infos(pubmed: pd.DataFrame, trials: pd.DataFrame, drug_str: str)
     """
     sub_pubmed = find_mention(pubmed, drug_str)
     sub_trials = find_mention(trials, drug_str)
-    journal_infos = get_journal_infos(sub_pubmed, sub_trials)
     pubmed_articles, trials_articles = get_article_infos(
         sub_pubmed), get_article_infos(sub_trials)
+    journal_infos = get_journal_infos(sub_pubmed, sub_trials)
     return dict(pubmed_articles=pubmed_articles, journal=journal_infos, trials=trials_articles)
 
 
--- src/test_graph/graph/utils.py
+++ src/test_graph/graph/utils.py
@@ -4,13 +4,38 @@ import pandas as pd
 
 
 def find_mention(df: pd.DataFrame, drug_str: str) -> pd.DataFrame:
+    """get subset of data that mentions drug
+
+    :param df: pubmed or clinical trials data
+    :type df: pd.DataFrame
+    :param drug_str: name of considered drug
+    :type drug_str: str
+    :return: subset of data that mentions drug
+    :rtype: pd.DataFrame
+    """
     return df[df["title"].str.contains(drug_str)]
 
 
 def get_article_infos(mention_subset: pd.DataFrame) -> Dict[Any, str]:
+    """get article infos as dict from subset data
+
+    :param mention_subset: subset of data mentioning some drug
+    :type mention_subset: pd.DataFrame
+    :return: article infos as dict
+    :rtype: Dict[Any, str]
+    """
     return mention_subset[["title", "date"]].to_dict()
 
 
 def get_journal_infos(mention_subset_pubmed: pd.DataFrame, mention_subset_trials: pd.DataFrame) -> Dict[Any, str]:
+    """get journal infos as dict from both pubmed and clinical trials subsets
+
+    :param mention_subset_pubmed: pubmed data subset mentioning some drug
+    :type mention_subset_pubmed: pd.DataFrame
+    :param mention_subset_trials: clinical trials subset mentioning some drug
+    :type mention_subset_trials: pd.DataFrame
+    :return: infos on journals mentioning some drug
+    :rtype: Dict[Any, str]
+    """
     subset = pd.concat([mention_subset_pubmed, mention_subset_trials])
     return subset[["journal", "date"]].groupby(["journal", "date"]).size().to_frame("size").reset_index().to_dict()
